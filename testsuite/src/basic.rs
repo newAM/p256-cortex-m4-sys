57ded4754647809fba9f013a8842b81bfbaa5e80
Alex Martens <alex@thinglab.org>
2022-07-02 14:43:05 -0700

    Add test for P256_check_range_p

--- testsuite/src/basic.rs
+++ testsuite/src/basic.rs
@@ -20,6 +20,9 @@ defmt::timestamp!("{=u32:us}", DWT::cycle_count() / CYC_PER_MICRO);
 mod tests {
     use super::*;
 
+    const ZERO: [u32; 8] = [0; 8];
+    const ONE: [u32; 8] = [0, 0, 0, 0, 0, 0, 0, 1];
+
     #[init]
     fn init() {
         let mut cp: pac::CorePeripherals = unwrap!(pac::CorePeripherals::take());
@@ -38,15 +41,13 @@ mod tests {
     fn check_range_n() {
         use p256_cortex_m4_sys::P256_check_range_n;
 
-        const ZERO: [u32; 8] = [0; 8];
         let valid: bool = unsafe { P256_check_range_n(ZERO.as_ptr()) };
-        defmt::assert!(!valid, "zero is not in range");
+        defmt::assert!(!valid, "0 is not in range");
 
-        const ONE: [u32; 8] = [0, 0, 0, 0, 0, 0, 0, 1];
         let valid: bool = unsafe { P256_check_range_n(ONE.as_ptr()) };
         defmt::assert!(valid, "1 is in range");
 
-        // n = 2**256 - 2**224 + 2**192 - int("4319055258e8617b0c46353d039cdaaf", 16)
+        // 2**256 - 2**224 + 2**192 - 0x4319055258e8617b0c46353d039cdaaf
         const N: [u32; 8] = [
             0xfc632551, 0xf3b9cac2, 0xa7179e84, 0xbce6faad, 0xffffffff, 0xffffffff, 0x00000000,
             0xffffffff,
@@ -59,6 +60,32 @@ mod tests {
             0xffffffff,
         ];
         let valid: bool = unsafe { P256_check_range_n(N_MINUS_ONE.as_ptr()) };
-        defmt::assert!(valid, "N_MINUS_ONE is within range");
+        defmt::assert!(valid, "N - 1 is within range");
+    }
+
+    #[test]
+    fn check_range_p() {
+        use p256_cortex_m4_sys::P256_check_range_p;
+
+        let valid: bool = unsafe { P256_check_range_p(ZERO.as_ptr()) };
+        defmt::assert!(valid, "0 is in range");
+
+        let valid: bool = unsafe { P256_check_range_p(ONE.as_ptr()) };
+        defmt::assert!(valid, "1 is in range");
+
+        // 2**256 - 2**224 + 2**192 + 2**96 - 1
+        const P: [u32; 8] = [
+            0xffffffff, 0xffffffff, 0xffffffff, 0x00000000, 0x00000000, 0x00000000, 0x00000001,
+            0xffffffff,
+        ];
+        let valid: bool = unsafe { P256_check_range_p(P.as_ptr()) };
+        defmt::assert!(!valid, "P is not within range");
+
+        const P_MINUS_ONE: [u32; 8] = [
+            0xfffffffe, 0xffffffff, 0xffffffff, 0x00000000, 0x00000000, 0x00000000, 0x00000001,
+            0xffffffff,
+        ];
+        let valid: bool = unsafe { P256_check_range_p(P_MINUS_ONE.as_ptr()) };
+        defmt::assert!(valid, "P - 1 is within range");
     }
 }
